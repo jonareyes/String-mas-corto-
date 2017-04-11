93b29eeb1e1a135b48820bd460e6b62b9b58b913
jonareyes <jonathan_reyesv@hotmail.com>
2017-04-10 20:57:34 -0500

    This is the complete exercise, this is correct

--- string_mas_corto_de_una_lista.rb
+++ string_mas_corto_de_una_lista.rb
@@ -1,15 +1,20 @@
 #In this method let's to create a shortest method that received one array of string and return the string or strings more shortest.
  	
 def shortest(array)
-	p word_short = []
-	array.each do |word| 
-		if word.length <= array.first.length
-		p word_short << word
-		end
-	end
-		word_short
+ arr = []
+ min = array[0].length
+ 	for i in 0..array.length-1
+  	if array[i].length <= min
+    	min = array[i].length
+  	end
+ 	end
+ 	array.each do |palabra|
+   arr << palabra if palabra.length == min
+ 	end
+ 
+ 	arr
 end
 
 p shortest(['uno', 'dos', 'tres', 'cuatro', 'cinco']) == ["uno", "dos"]
 p shortest(['gato', 'perro', 'elefante', 'jirafa']) == ["gato"]
-p shortest(['rojo', 'verde', 'negro', 'morado']) == ["rojo"]
+p shortest(['verde','rojo' ,'negro', 'morado']) == ["rojo"]
